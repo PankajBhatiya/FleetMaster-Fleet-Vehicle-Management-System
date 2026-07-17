00b2bdf60b6cf287424ef457a541c51865bc834a
Tanishq Uppal <tanishquppal220@gmail.com>
2026-07-17 17:17:54 +0530

    1. Removed Redundent Dependences 2. Fixed a Bug due to worng import 3.  added a Example env file

--- Backend/src/Services/notificationService.js
+++ Backend/src/Services/notificationService.js
@@ -1,4 +1,4 @@
-import Notification from '../models/Notification.js';
+import Notification from '../Models/Notification.js';
 
 // Dispatch a notification
 export const dispatchNotification = async ({ 
